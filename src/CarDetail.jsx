4b18fc3dca92993b2df446c683f598ea2ab5ac44
Elisha Bere <elishabere4@gmail.com>
2022-07-03 00:23:15 +0200

    carDetail info cleanup

--- src/CarDetail.jsx
+++ src/CarDetail.jsx
@@ -4,60 +4,67 @@ import { updateResponseKeyString } from "./components/utils.js";
 const CarDetails = (props) => {
   const { carId } = useParams();
   var carsData = props.availableCarsToRentInfo;
+  var carInfo;
 
   const selectedCar = carsData.find(
     (car) => updateResponseKeyString(car.Vehicle).Code === carId
   );
+
+  carInfo = updateResponseKeyString(selectedCar.Vehicle);
+
   const selectedCarName = updateResponseKeyString(
     selectedCar.Vehicle.VehMakeModel
   ).Name;
 
+
+
+  console.log(carInfo.pictureURL)
+  
+
   return (
     <>
       <div>
-        <h1 className="inline-block text-2xl font-semibold sm:text-3xl">{selectedCarName}</h1>
+      
       </div>
       <a
         rel="noopener noreferrer"
         href="/"
         className="flex items-center flex-shrink-0 px-5 py-2 border-b-4 dark:border-violet-400  dark:text-gray-50"
-      > </a>
+      >
+        {" "}
+      </a>
 
-    
-
-      <div className="  mx-auto sm:p-10 md:p-16 dark:text-gray-100">
-        <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-          <img
-            src="https://source.unsplash.com/random/480x360"
-            alt=""
-            className="w-full h-60 sm:h-96 dark:bg-gray-500"
-          />
-          <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-900">
-            <div className="space-y-2">
-              <a
-                rel="noopener noreferrer"
-                href="/"
-                className="inline-block text-2xl font-semibold sm:text-3xl"
-              >
-                The Best Activewear from the Nordstrom Anniversary Sale
-              </a>
-              <p className="text-xs dark:text-gray-400">
-                By
-                <a
-                  rel="noopener noreferrer"
-                  href="/"
-                  className="text-xs hover:underline"
-                >
-                  Leroy Jenkins
-                </a>
-              </p>
-            </div>
-            <div className="dark:text-gray-100">
-              <p>Insert the actual text content here...</p>
-            </div>
+      <section className="p-6 dark:text-gray-100">
+        <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
+          <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
+            <span className="block mb-2 dark:text-violet-400">
+              {selectedCarName}
+            </span>
+            <h1 className="text-3xl font-extrabold dark:text-gray-50">
+            {selectedCarName}
+            </h1>
+            <p className="my-8">
+              <span className="font-medium dark:text-gray-50">
+                Modular and versatile.
+              </span>
+              Fugit vero facilis dolor sit neque cupiditate minus esse accusamus
+              cumque at.
+            </p>
+            <button
+              type="button"
+              className="w-full py-2 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
+            >
+              CHOOSE
+            </button>
           </div>
+          <img
+            src={carInfo.PictureURL}
+            alt=""
+            className="w-full rounded-md xl:col-span-3 dark:bg-gray-500"
+          />
+          
         </div>
-      </div>
+      </section>
     </>
   );
 };
