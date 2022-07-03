f28bef06acb2998191c3ee8fa26c13db46ea25a1
Elisha Bere <elishabere4@gmail.com>
2022-07-03 02:21:54 +0200

    minimal display details page done

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -19,33 +19,33 @@ const CarCard = (props) => {
           {" "}
           {props.carName.Name}{" "}
         </div>
-        <p className="inline-flex text-lg p-1">
+        <p className="inline-flex text-md p-0">
           {props.carTotalCharges.CurrencyCode}{" "}
         </p>
         <p className="inline-flex p-2 text-xl font-bold rounded-md text-violet-300">
           {props.carTotalCharges.EstimatedTotalAmount}{" "}
         </p>
-        <p className="inline-flex text-sm">(estimate total) </p>
+        <p className="inline-flex text-xs">(estimate total) </p>
       </div>
 
-      <div className="px-3 pb-2 ">
-        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">
+      <div className="px-3 pb-2 text-center">
+        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-normal text-gray-200 mr-2 mb-2">
           {props.carInfo.PassengerQuantity} Passengers
         </span>
-        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">
+        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-normal text-gray-200 mr-2 mb-2">
           {props.carInfo.DoorCount} Doors
         </span>
-        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">
+        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-normal text-gray-200 mr-2 mb-2">
           {props.carInfo.TransmissionType}
         </span>
 
-        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">
+        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-normal text-gray-200 mr-2 mb-2">
           {props.carInfo.AirConditionInd ? "AC" : "No AC"}
         </span>
-        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">
+        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-normal text-gray-200 mr-2 mb-2">
           {props.carInfo.FuelType}
         </span>
-        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">
+        <span className="shadow-lg shadow-indigo-500/10 inline-block  rounded-full px-3 py-1 text-sm font-normal text-gray-200 mr-2 mb-2">
           Buggage Quantity #{props.carInfo.BaggageQuantity}
         </span>
       </div>
@@ -53,7 +53,7 @@ const CarCard = (props) => {
       <div className="px-8 pt-4 pb-5 grid grid-cols-1 content-center ">
         <Link
           to={props.carDetailURL}
-          className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-violet-800 bg-white hover:bg-violet-50"
+          className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-sm font-medium rounded-md text-violet-800 bg-white hover:bg-violet-50"
         >
           {" "}
           More Details{" "}
--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import { updateResponseKeyString } from "../helpers/utils.js";
+import {
+  Link
+} from "react-router-dom";
 
 const CarDetails = (props) => {
   const { carId } = useParams();
@@ -9,20 +12,27 @@ const CarDetails = (props) => {
   const selectedCar = carsData.find(
     (car) => updateResponseKeyString(car.Vehicle).Code === carId
   );
-  
+
   const selectedCarName = updateResponseKeyString(
     selectedCar.Vehicle.VehMakeModel
-  ).Name;
+  );
+  console.log(selectedCar.TotalCharge);
 
   carInfo = updateResponseKeyString(selectedCar.Vehicle);
 
-  
-
   return (
     <>
-      <div>
+      <div className="px-2 text-left ">
       
-      </div>
+      <Link to="/" className="inline-flex text-md">Home {" "}</Link>
+     
+      <p className="inline-flex pl-2 pr-2 text-md text-white"> / </p>
+      <p className="inline-flex  text-lg font-bold rounded-md text-violet-200">
+        Cars{" "}
+      </p> 
+      
+     
+    </div>
       <a
         rel="noopener noreferrer"
         href="/"
@@ -35,31 +45,56 @@ const CarDetails = (props) => {
         <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
           <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
             <span className="block mb-2 dark:text-violet-400">
-              {selectedCarName}
+              {selectedCar.Status}
             </span>
             <h1 className="text-3xl font-extrabold dark:text-gray-50">
-            {selectedCarName}
+              {selectedCarName.Name}
             </h1>
-            <p className="my-8">
-              <span className="font-medium dark:text-gray-50">
-                Modular and versatile.
+
+            {/* / */}
+            <div className="px-2 text-center ">
+      
+              <p className="inline-flex text-md ">
+                {updateResponseKeyString(selectedCar.TotalCharge).CurrencyCode}{" "}
+              </p>
+              <p className="inline-flex p-2 text-xl font-bold rounded-md text-violet-300">
+                {updateResponseKeyString(selectedCar.TotalCharge).EstimatedTotalAmount}{" "}
+              </p>
+              <p className="inline-flex text-xs">(estimate total) </p>
+            </div>
+
+            <div className="px-2 ">
+              <span className="uppercase shadow-lg shadow-indigo-500/10 inline-block px-10 py-4 text-sm font-semibold text-gray-200 mr-0 mb-2">
+                {carInfo.PassengerQuantity} Passengers
               </span>
-              Fugit vero facilis dolor sit neque cupiditate minus esse accusamus
-              cumque at.
-            </p>
-            <button
+              <span className=" uppercase shadow-lg shadow-indigo-500/10 inline-block px-10 py-4 text-sm font-semibold text-gray-200 mr-1 mb-2">
+                {carInfo.DoorCount} Doors
+              </span>
+
+              <span className="uppercase shadow-lg shadow-indigo-500/10 inline-block px-10 py-4 text-sm font-semibold text-gray-200 mr-1 mb-2">
+                {carInfo.TransmissionType}
+              </span>
+
+              <span className="uppercase shadow-lg shadow-indigo-500/10 inline-block px-10 py-4 text-sm font-semibold text-gray-200 mr-1 mb-2">
+                {carInfo.FuelType}
+              </span>
+              <span className="uppercase shadow-lg shadow-indigo-500/10 inline-block px-12 py-4 text-sm font-semibold text-gray-200 mr-1 mb-2">
+                {carInfo.AirConditionInd ? "Air Con" : "No AC"}
+              </span>
+            </div>
+
+            {/* <button
               type="button"
               className="w-full py-2 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
             >
               CHOOSE
-            </button>
+            </button> */}
           </div>
           <img
             src={carInfo.PictureURL}
             alt=""
             className="w-full rounded-md xl:col-span-3 dark:bg-gray-500"
           />
-          
         </div>
       </section>
     </>
