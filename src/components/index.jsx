ce41ecaf1a6681f1dfff975a8d1a84a416696036
Elisha Bere <elishabere4@gmail.com>
2022-07-04 02:23:16 +0200

    remove com unused

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,4 @@ export { default as CarCard } from "./CarCard.jsx";
 export { default as DisplayCars } from "./DisplayCars.jsx";
 export { default as FilterByVendor } from "./Filter.jsx";
 export { default as Footer } from "./Footer.jsx";
-export { default as Legend } from "./Legend.jsx";
 
