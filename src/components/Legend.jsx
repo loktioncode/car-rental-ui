c94e7f4e2f5bc71f82f7e4af9cc4359170c62949
Elisha Bere <elishabere4@gmail.com>
2022-07-05 01:35:40 +0200

    clean up and fix breakpoints

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -3,7 +3,7 @@ import { updateResponseKeyString, formartDate } from "../helpers/utils";
 const Legend = (props) => {
   return (
     <legend className="sticky top-0 container max-w-6xl  mx-auto mt-5 rounded-lg shadow-lg bg-slate-900/80 text-gray-200 bg-slate-900 bg-opacity-60 backdrop-blur-lg">
-      <div className="md:flex justify-around sm:flex-wrap py-6 px-1">
+      <div className="md:flex justify-around sm:flex-wrap py-6 px-5 md:px-2">
         <div className="md:flex-2 md:px-2 sm:text-center">
 
 
@@ -11,7 +11,7 @@ const Legend = (props) => {
             updateResponseKeyString(props.pickUpTimeData.PickUpLocation).Name
               === updateResponseKeyString(props.pickUpTimeData.ReturnLocation).Name ? (
               <div className="">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
@@ -37,11 +37,11 @@ const Legend = (props) => {
           }
 
         </div>
-        <div className="flex-3 md:flex-1">
+        <div className="flex-3 md:flex-1 px-0 md:px-5">
 
           <div>
 
-            <p className="text-xs inline-block lg:uppercase">PickUp Time :</p>
+            <p className="text-xs inline-block lg:uppercase md:pr-1">PickUp Time :</p>
             <span className="text-sm md:text-base inline-block lg:uppercase">{formartDate(props.pickUpTimeData.PickUpDateTime)}</span>
           </div>
 
@@ -51,7 +51,7 @@ const Legend = (props) => {
         <div className="flex-3 md:flex-1">
 
           <div>
-            <p className="text-xs inline-block lg:uppercase">Return Time :</p>
+            <p className="text-xs inline-block lg:uppercase md:pr-1">Return Time :</p>
             <span className="text-sm md:text-base inline-block lg:uppercase ">{formartDate(props.pickUpTimeData.ReturnDateTime)}</span>
           </div>
 
