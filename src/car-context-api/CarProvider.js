32376892f34931eacf7b58d90af57032d30153b0
Elisha Bere <elishabere4@gmail.com>
2022-07-04 02:24:32 +0200

    rmv unused import

--- src/car-context-api/CarProvider.js
+++ src/car-context-api/CarProvider.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from "react";
+import { useState } from "react";
 import { allCars } from "./car_data";
 import CarContext from "./CarContext";
 
@@ -7,28 +7,6 @@ export const CarProvider = ({ children }) => {
   const [cars, setCars] = useState(allCars);
   const [filteredCarsByVendor, setVendorFilterByVendor] = useState([]);
   const [showFilter, setFilter] = useState(false);
-
-
-
-  //ANOTHER WAY TO GET CARS DYNAMICALLY
-  // const getCars = async () => {
-  //   const response = await fetch("http://www.cartrawler.com/ctabe/cars.json", {
-  //     mode: "cors",
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*",
-  //     },
-  //   });
-
-  //   if (!response.ok) {
-  //     const message = `An error has occured: ${response.status}`;
-  //     throw new Error(message);
-  //   }
-  // };
-
-  // useEffect(() => {
-    // const allCars = getCars();
-    // setCars(allCars);
-  // }, []);
 
   const filterCarsByVendor = (data) => {
     setVendorFilterByVendor(data);
