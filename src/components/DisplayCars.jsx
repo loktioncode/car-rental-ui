6ebd774f9e6c0824ba109aec3f373464d6c214fa
Elisha Bere <elishabere4@gmail.com>
2022-07-07 00:56:02 +0200

    fix price filter text:trick ..lol

--- src/components/DisplayCars.jsx
+++ src/components/DisplayCars.jsx
@@ -3,7 +3,7 @@ import FilterByVendor from "./FilterCarsByVendor";
 import Legend from "./Legend";
 import FilterByPrice from "./FilterCarsByPrice";
 import { updateResponseKeyString } from "../helpers/utils";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import CarContext from "../car-context-api/CarContext";
 import displayStyle from '../styles/displaycars.module.css';
 import secondaryBtnStyle from '../styles/buttons.module.css';
@@ -21,113 +21,141 @@ const DisplayCars = (props) => {
   const availableVendors = props.availableVendors;
   const availableCarsToRentInfo = props.availableCarsToRentInfo;
 
-  const handleFilterByNumber = (number) => setCarsByNumber(availableCarsToRentInfo.slice(0, number));
+  const [sortByPrice, setSortByPrice] = useState('Sort by price: low to high');
+
+  const handleFilterByNumber = useCallback((number) => {
+    setCarsByNumber(availableCarsToRentInfo.slice(0, number))
+  }, [availableCarsToRentInfo]);
 
 
-  const filterByLowest = (lowest) => {
+  const options = [
+    { label: 'Sort by price: low to high', value: '1' },
+    { label: 'Sort by price: high to low', value: '2' }
+  ];
 
-    console.log(`Filter byHighest: ${lowest}`);
+  const onPriceFilterChange = (event) => {
+    setSortByPrice(event.target.value);
+
   };
 
-  const filterByHighest = (highest) => {
-    console.log(`Filter byHighest: ${highest}`);
-    // if (availableCarsToRentInfo.length !== 0) {
-    //   availableCarsToRentInfo.sort(function (a, b) {
-    //     return (
-    //       parseInt(updateResponseKeyString(b.TotalCharge).EstimatedTotalAmount) -
-    //       parseInt(updateResponseKeyString(a.TotalCharge).EstimatedTotalAmount) 
-         
-    //     );
-    //   });
-    // }
-  
-  };
+
+  const filterByPrice = useCallback(() => {
+    if (sortByPrice === 'Sort by price: low to high') {
+     
+      availableCarsToRentInfo.sort(function (a, b) {
+        return (
+          parseInt(updateResponseKeyString(a.TotalCharge).EstimatedTotalAmount) -
+          parseInt(updateResponseKeyString(b.TotalCharge).EstimatedTotalAmount)
+
+        );
+      });
+      handleFilterByNumber(6);
+
+    } else {
+      
+      availableCarsToRentInfo.sort(function (a, b) {
+        return (
+          parseInt(updateResponseKeyString(b.TotalCharge).EstimatedTotalAmount) -
+          parseInt(updateResponseKeyString(a.TotalCharge).EstimatedTotalAmount)
+
+        );
+      });
+      handleFilterByNumber(6);
+    }
+  }, [availableCarsToRentInfo, sortByPrice, handleFilterByNumber]);
 
 
   useEffect(() => {
-    handleFilterByNumber(6);
-  }, []);
+    filterByPrice();
+  }, [handleFilterByNumber, filterByPrice]);
 
   // console.log(props.pickUpTimeData)
 
   return (
 
-      <section className={displayStyle.displayCars}>
-        {availableVendors.length !== 0 ? (
-          <div className={displayStyle.filterContainer}>
-            <button
-              data-test="instructions"
-              onClick={() => {
-                filterCarsByVendor([]);
-                setFilter(false);
-              }}
-              className={displayStyle.allVendorsBtn + ' ' + 'semibold'}
-            >
-              ALL
-            </button>
-            {availableVendors.map((vendor) => (
-              <FilterByVendor
-                key={vendor.Code}
-                vendorName={vendor.Name}
-                allCars={allCars}
-                vendorCode={vendor.Code}
+    <section className={displayStyle.displayCars}>
+      {availableVendors.length !== 0 ? (
+        <div className={displayStyle.filterContainer}>
+          <button
+            data-test="instructions"
+            onClick={() => {
+              filterCarsByVendor([]);
+              setFilter(false);
+            }}
+            className={displayStyle.allVendorsBtn + ' ' + 'semibold'}
+          >
+            ALL
+          </button>
+          {availableVendors.map((vendor) => (
+            <FilterByVendor
+              key={vendor.Code}
+              vendorName={vendor.Name}
+              allCars={allCars}
+              vendorCode={vendor.Code}
+            />
+          ))}
+
+
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+
+
+      <FilterByPrice options={options} onChange={onPriceFilterChange} value={sortByPrice} />
+
+
+      <Legend pickUpTimeData={props.pickUpTimeData} />
+      <div className={displayStyle.carsContainer}>
+
+        <div className={displayStyle.carGrid}>
+          {availableCarsToRentInfo.length !== 0 &&
+            !showFiltered &&
+            availableCarsToRentFilteredByNumber.length !== 0
+            ? availableCarsToRentFilteredByNumber.map((car) => (
+              <CarCard
+                key={updateResponseKeyString(car.Vehicle).Code}
+                carInfo={updateResponseKeyString(car.Vehicle)}
+                carName={updateResponseKeyString(car.Vehicle.VehMakeModel)}
+                carTotalCharges={updateResponseKeyString(car.TotalCharge)}
+                carDetailURL={`/rentalcars/${updateResponseKeyString(car.Vehicle).Code}`}
+              />
+            ))
+            : filteredCarsByVendor.map((car) => (
+              <CarCard
+                key={updateResponseKeyString(car.Vehicle).Code}
+                carInfo={updateResponseKeyString(car.Vehicle)}
+                carName={updateResponseKeyString(car.Vehicle.VehMakeModel)}
+                carTotalCharges={updateResponseKeyString(car.TotalCharge)}
+                carDetailURL={`/rentalcars/${updateResponseKeyString(car.Vehicle).Code}`}
               />
             ))}
+        </div>
+
+        <div className="mt-8">
 
 
-          </div>
-        ) : (
-          <div>Loading...</div>
-        )}
-        <FilterByPrice low={filterByLowest} high={filterByHighest} />
-        <Legend pickUpTimeData={props.pickUpTimeData} />
-        <div className={displayStyle.carsContainer}>
 
-          <div className={displayStyle.carGrid}>
-            {availableCarsToRentInfo.length !== 0 &&
-              !showFiltered &&
-              availableCarsToRentFilteredByNumber.length !== 0
-              ? availableCarsToRentFilteredByNumber.map((car) => (
-                <CarCard
-                  key={updateResponseKeyString(car.Vehicle).Code}
-                  carInfo={updateResponseKeyString(car.Vehicle)}
-                  carName={updateResponseKeyString(car.Vehicle.VehMakeModel)}
-                  carTotalCharges={updateResponseKeyString(car.TotalCharge)}
-                  carDetailURL={`/rentalcars/${updateResponseKeyString(car.Vehicle).Code}`}
-                />
-              ))
-              : filteredCarsByVendor.map((car) => (
-                <CarCard
-                  key={updateResponseKeyString(car.Vehicle).Code}
-                  carInfo={updateResponseKeyString(car.Vehicle)}
-                  carName={updateResponseKeyString(car.Vehicle.VehMakeModel)}
-                  carTotalCharges={updateResponseKeyString(car.TotalCharge)}
-                  carDetailURL={`/rentalcars/${updateResponseKeyString(car.Vehicle).Code}`}
-                />
-              ))}
-          </div>
-
-          <div className="mt-8">
-            {availableCarsToRentFilteredByNumber.length !== props.availableCarsToRentInfo.length && !showFiltered ? (
-              <button
-                type="button"
-                className={ secondaryBtnStyle.secondaryBtn}
-                onClick={() => {
-                  handleFilterByNumber(props.availableCarsToRentInfo.length);
-                }}
-              >
-                Load More Cars...
-              </button>
-            ) : (
-              ""
-            )}
-          </div>
-
-      
+          {availableCarsToRentFilteredByNumber.length !== props.availableCarsToRentInfo.length && !showFiltered ? (
+            <button
+              type="button"
+              className={secondaryBtnStyle.secondaryBtn}
+              onClick={() => {
+                handleFilterByNumber(props.availableCarsToRentInfo.length);
+              }}
+            >
+              Load More Cars...
+            </button>
+          ) : (
+            ""
+          )}
         </div>
 
 
-      </section>
+      </div>
+
+
+    </section>
 
   );
 };
--- src/components/FilterCarsByPrice.jsx
+++ src/components/FilterCarsByPrice.jsx
@@ -1,25 +1,19 @@
-import React, { useContext, useEffect, useState } from "react";
+
 import filterStyle from '../styles/filterbyprice.module.css';
 
 
-const FilterByPrice = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggling = () => setIsOpen(!isOpen);
-
+const FilterByPrice = ({ value, options, onChange }) => {
 
     return (
-        <>
-            <form class={filterStyle.filterBox} method="get">
-                <select name="orderby" class="orderby" onClick={toggling}>
-                <option value="price" selected="selected">Default Sorting</option>
-                <option value="price-asc"  onClick={() => props.low("ss")}>Sort by price: low to high</option>
-                                <option value="price-desc">Sort by price: high to low</option>
 
-                </select>
-                <input type="hidden" name="paged" value="1"></input>
-            </form>
-        </>
+        <div className={filterStyle.filterBox} >
+            <select value={value} onChange={onChange} >
+                {options.map((option) => (
+                    <option key={Math.random()} value={options.value}>{option.label}</option>
+                ))}
+            </select>
+        </div>
+
     );
 };
 
